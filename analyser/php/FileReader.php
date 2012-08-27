96afee39aa5d153a9155defed32f7fdbdfe56e9d
andreme@server <andreme@server>
2012-08-27 12:14:16 +1000

    move Reader baseclass to own file

--- analyser/php/FileReader.php
+++ analyser/php/FileReader.php
@@ -1,41 +1,5 @@
 <?php
 
-class Reader {
-
-	private $eof = false;
-
-	private $line;
-
-	protected $handle;
-
-	private $lineNo = 0;
-
-	public function nextLine() {
-		$this->line = fgets($this->handle);
-		$this->eof = $this->line === false;
-		if (!$this->eof) {
-			$this->lineNo++;
-		}
-	}
-
-	public function getLine() {
-		return rtrim($this->line);
-	}
-
-	public function isEof() {
-		return $this->eof;
-	}
-
-	public function __destruct() {
-		fclose($this->handle);
-	}
-
-	public function getLineNo() {
-		return $this->lineNo;
-	}
-
-}
-
 class FileReader extends Reader {
 
 	private $filename;
